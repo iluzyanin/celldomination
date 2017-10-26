3270bda8bb9d06a766f7e6550e05b9d234dde732
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2017-10-26 20:32:58 +0200

    Refactor class calculation for cells

--- src/components/cell/cell.jsx
+++ src/components/cell/cell.jsx
@@ -3,19 +3,28 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Cell = (props) => {
+    const borderClassName = (border) => {
+        let cssClass = border;
+        if (props[border] === 2) {
+            cssClass += ' active';
+        }
+
+        return cssClass;
+    };
+
     return (
         <div className={classNames('box', { 'isFirstPlayer': props.isActive })}>
             {props.top > 0 &&
-                <div className={classNames('top', { 'active': props.top === 2 })} onClick={() => props.onBorderClick(0)}></div>
+                <div className={borderClassName('top')} onClick={() => props.onBorderClick(0)}></div>
             }
             {props.right > 0 &&
-                <div className={classNames('right', { 'active': props.right === 2 })} onClick={() => props.onBorderClick(1)}></div>
+                <div className={borderClassName('right')} onClick={() => props.onBorderClick(1)}></div>
             }
             {props.bottom > 0 &&
-                <div className={classNames('bottom', { 'active': props.bottom === 2 })} onClick={() => props.onBorderClick(2)}></div>
+                <div className={borderClassName('bottom')} onClick={() => props.onBorderClick(2)}></div>
             }
             {props.left > 0 &&
-                <div className={classNames('left', { 'active': props.left === 2 })} onClick={() => props.onBorderClick(3)}></div>
+                <div className={borderClassName('left')} onClick={() => props.onBorderClick(3)}></div>
             }
         </div>
     );
