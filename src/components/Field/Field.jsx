a45470f05a95d380d09b24e359e78c5f1c65a4a8
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2017-12-13 21:13:17 +0100

    Introduce Game Over condition

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -5,187 +5,186 @@ import Cell from '../Cell/Cell';
 import buildRows from '../../models/FieldModel';
 import classNames from 'classnames';
 
-class Field extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rows: [],
-            player: 0,
-            playerOneScore: 0,
-            playerTwoScore: 0
+class Field extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [],
+      player: 0,
+      playerOneScore: 0,
+      playerTwoScore: 0,
+      isGameOver: false
+    }
+    this.activateBorder = this.activateBorder.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ rows: buildRows(3) });
+  }
+
+  render() {
+    return (
+      <div className={classNames('container', { 'container--animated': this.state.isContainerAnimated })}>
+        {this.state.rows.map((cells, rowIndex) =>
+          <div className="row" key={rowIndex}>
+            {cells.map((cell, cellIndex) =>
+              <Cell key={cellIndex} onBorderClick={(borderIndex) => this.activateBorder(rowIndex, cellIndex, borderIndex)} {...cell} />)}
+          </div>)}
+        <p>Current player: {this.state.player}</p>
+        <p>Player one score: {this.state.playerOneScore}</p>
+        <p>Player two score: {this.state.playerTwoScore}</p>
+        {this.state.isGameOver && 
+        <p>Game over! {this.state.playerOneScore > this.state.playerTwoScore ? 'You won :)' : 'Computer won :('}</p>
         }
-        this.activateBorder = this.activateBorder.bind(this);
-    }
+      </div>
+    );
+  }
 
-    componentDidMount() {
-        this.setState({ rows: buildRows(7) });
-    }
-
-    render() {
-        return (
-            <div className={classNames('container', { 'container--animated': this.state.isContainerAnimated })}>
-                {this.state.rows.map((cells, rowIndex) =>
-                    <div className="row" key={rowIndex}>
-                        {cells.map((cell, cellIndex) =>
-                            <Cell key={cellIndex} onBorderClick={(borderIndex) => this.activateBorder(rowIndex, cellIndex, borderIndex)} {...cell} />)}
-                    </div>)}
-                <p>Current player: {this.state.player}</p>
-                <p>Player one score: {this.state.playerOneScore}</p>
-                <p>Player two score: {this.state.playerTwoScore}</p>
-            </div>
-        );
-    }
-
-    activateBorder(row, column, border) {
-        this.setState(prevState => {
-            let rows = prevState.rows;
-            const currentCell = rows[row][column];
-            let keepTurn = false;
-            if (border === 0) {
-                if (currentCell.top === 2) {
-                    return;
-                }
-                rows = update(rows, { [row]: { [column]: { top: { $set: 2 } } } });
-                rows = update(rows, { [row - 1]: { [column]: { bottom: { $set: 2 } } } });
-                if (rows[row][column].isActive) {
-                    rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-                if (rows[row - 1][column].isActive) {
-                    rows = update(rows, { [row - 1]: { [column]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-            }
-            if (border === 1) {
-                if (currentCell.right === 2) {
-                    return;
-                }
-                rows = update(rows, { [row]: { [column]: { right: { $set: 2 } } } });
-                rows = update(rows, { [row]: { [column + 1]: { left: { $set: 2 } } } });
-                if (rows[row][column].isActive) {
-                    rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-                if (rows[row][column + 1].isActive) {
-                    rows = update(rows, { [row]: { [column + 1]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-            }
-            if (border === 2) {
-                if (currentCell.bottom === 2) {
-                    return;
-                }
-                rows = update(rows, { [row]: { [column]: { bottom: { $set: 2 } } } });
-                rows = update(rows, { [row + 1]: { [column]: { top: { $set: 2 } } } });
-                if (rows[row][column].isActive) {
-                    rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-                if (rows[row + 1][column].isActive) {
-                    rows = update(rows, { [row + 1]: { [column]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-            }
-            if (border === 3) {
-                if (currentCell.left === 2) {
-                    return;
-                }
-                rows = update(rows, { [row]: { [column]: { left: { $set: 2 } } } });
-                rows = update(rows, { [row]: { [column - 1]: { right: { $set: 2 } } } });
-                if (rows[row][column].isActive) {
-                    rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-                if (rows[row][column - 1].isActive) {
-                    rows = update(rows, { [row]: { [column - 1]: { player: { $set: prevState.player } } } });
-                    keepTurn = true;
-                }
-            }
-
-            let playerOneScore = keepTurn && prevState.player === 0 ? prevState.playerOneScore + 1 : prevState.playerOneScore;
-            let playerTwoScore = keepTurn && prevState.player === 1 ? prevState.playerTwoScore + 1 : prevState.playerTwoScore;
-            let player = keepTurn ? prevState.player : prevState.player === 0 ? 1 : 0;
-
-            if (player === 1) {
-                setTimeout(() => {
-                    this.computerMove();
-                }, 100);
-            }
-
-            return { rows, player, playerOneScore, playerTwoScore };
-        });
-
-        /*if (rows.some(function (row) {
-            return row.some(function (cell) {
-                return cell.top === 1 ||
-                    cell.right === 1 ||
-                    cell.bottom === 1 ||
-                    cell.left === 1;
-            });
-        })) {
-            if (!keepTurn) {
-                $scope.currentPlayer = $scope.currentPlayer === 0 ? 1 : 0;
-            }
-
-            if ($scope.currentPlayer === 1) {
-                $timeout(function () {
-                    computerMove();
-                }, 100);
-            }
-        } else {
-            $scope.isGameOver = true;
-        }*/
-    }
-
-    computerMove() {
-        const availableMoves = [];
-        const size = this.state.rows.length;
-        for (let i = 0; i < size; i++) {
-            for (let j = 0; j < size; j++) {
-                const cell = this.state.rows[i][j];
-                if (cell.player !== undefined) {
-                    continue;
-                }
-                const openBorders = cell.getOpenBorders();
-
-                if (openBorders.length === 1) {
-                    this.activateBorder(i, j, openBorders[0]);
-                    return;
-                }
-                if (openBorders.length === 0) {
-                    continue;
-                }
-                for (let k = 0; k < openBorders.length; k++) {
-                    const isLowPriority = openBorders.length === 2 ||
-                        (i > 0 && openBorders[k] === 0 && this.state.rows[i - 1][j].getOpenBorders().length === 2) ||
-                        (j < size - 1 && openBorders[k] === 1 && this.state.rows[i][j + 1].getOpenBorders().length === 2) ||
-                        (i < size - 1 && openBorders[k] === 2 && this.state.rows[i + 1][j].getOpenBorders().length === 2) ||
-                        (j > 0 && openBorders[k] === 3 && this.state.rows[i][j - 1].getOpenBorders().length === 2);
-                    availableMoves.push({
-                        row: i,
-                        column: j,
-                        border: openBorders[k],
-                        isLowPriority: isLowPriority
-                    });
-                }
-            }
+  activateBorder(row, column, border) {
+    this.setState(prevState => {
+      let rows = prevState.rows;
+      const currentCell = rows[row][column];
+      let keepTurn = false;
+      let scoreIncrement = 0;
+      if (border === 0) {
+        if (currentCell.top === 2) {
+          return;
         }
-
-        if (availableMoves.length === 0) {
-            return;
+        rows = update(rows, { [row]: { [column]: { top: { $set: 2 } } } });
+        rows = update(rows, { [row - 1]: { [column]: { bottom: { $set: 2 } } } });
+        if (rows[row][column].isActive) {
+          rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
         }
-        var goodMoves = availableMoves.filter(function (cell) {
-            return !cell.isLowPriority;
-        });
-
-        if (goodMoves.length > 0) {
-            const randomMove = goodMoves[Math.floor(Math.random() * (goodMoves.length))];
-            this.activateBorder(randomMove.row, randomMove.column, randomMove.border);
-            return;
+        if (rows[row - 1][column].isActive) {
+          rows = update(rows, { [row - 1]: { [column]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
         }
-        const randomMove = availableMoves[Math.floor(Math.random() * (availableMoves.length))];
-        this.activateBorder(randomMove.row, randomMove.column, randomMove.border);
+      }
+      if (border === 1) {
+        if (currentCell.right === 2) {
+          return;
+        }
+        rows = update(rows, { [row]: { [column]: { right: { $set: 2 } } } });
+        rows = update(rows, { [row]: { [column + 1]: { left: { $set: 2 } } } });
+        if (rows[row][column].isActive) {
+          rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
+        }
+        if (rows[row][column + 1].isActive) {
+          rows = update(rows, { [row]: { [column + 1]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
+        }
+      }
+      if (border === 2) {
+        if (currentCell.bottom === 2) {
+          return;
+        }
+        rows = update(rows, { [row]: { [column]: { bottom: { $set: 2 } } } });
+        rows = update(rows, { [row + 1]: { [column]: { top: { $set: 2 } } } });
+        if (rows[row][column].isActive) {
+          rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
+        }
+        if (rows[row + 1][column].isActive) {
+          rows = update(rows, { [row + 1]: { [column]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
+        }
+      }
+      if (border === 3) {
+        if (currentCell.left === 2) {
+          return;
+        }
+        rows = update(rows, { [row]: { [column]: { left: { $set: 2 } } } });
+        rows = update(rows, { [row]: { [column - 1]: { right: { $set: 2 } } } });
+        if (rows[row][column].isActive) {
+          rows = update(rows, { [row]: { [column]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
+        }
+        if (rows[row][column - 1].isActive) {
+          rows = update(rows, { [row]: { [column - 1]: { player: { $set: prevState.player } } } });
+          keepTurn = true;
+          scoreIncrement++;
+        }
+      }
+
+      let playerOneScore = keepTurn && prevState.player === 0 ? prevState.playerOneScore + scoreIncrement : prevState.playerOneScore;
+      let playerTwoScore = keepTurn && prevState.player === 1 ? prevState.playerTwoScore + scoreIncrement : prevState.playerTwoScore;
+      let player = keepTurn ? prevState.player : prevState.player === 0 ? 1 : 0;
+
+      let areMovesExist = rows.some(cells => cells.some(cell => cell.hasMove));
+      let isGameOver = prevState.isGameOver;
+      if (areMovesExist) {
+        if (player === 1) {
+          setTimeout(() => {
+            this.computerMove();
+          }, 100);
+        }
+      } else {
+        isGameOver = true;
+      }
+
+      return { rows, player, playerOneScore, playerTwoScore, isGameOver };
+    });
+  }
+
+  computerMove() {
+    const availableMoves = [];
+    const size = this.state.rows.length;
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        const cell = this.state.rows[i][j];
+        if (cell.player !== undefined) {
+          continue;
+        }
+        const openBorders = cell.getOpenBorders();
+
+        if (openBorders.length === 1) {
+          this.activateBorder(i, j, openBorders[0]);
+          return;
+        }
+        if (openBorders.length === 0) {
+          continue;
+        }
+        for (let k = 0; k < openBorders.length; k++) {
+          const isLowPriority = openBorders.length === 2 ||
+            (i > 0 && openBorders[k] === 0 && this.state.rows[i - 1][j].getOpenBorders().length === 2) ||
+            (j < size - 1 && openBorders[k] === 1 && this.state.rows[i][j + 1].getOpenBorders().length === 2) ||
+            (i < size - 1 && openBorders[k] === 2 && this.state.rows[i + 1][j].getOpenBorders().length === 2) ||
+            (j > 0 && openBorders[k] === 3 && this.state.rows[i][j - 1].getOpenBorders().length === 2);
+          availableMoves.push({
+            row: i,
+            column: j,
+            border: openBorders[k],
+            isLowPriority: isLowPriority
+          });
+        }
+      }
     }
+
+    if (availableMoves.length === 0) {
+      return;
+    }
+    var goodMoves = availableMoves.filter(function (cell) {
+      return !cell.isLowPriority;
+    });
+    console.log(goodMoves.length, availableMoves.length)
+
+    if (goodMoves.length > 0) {
+      const randomMove = goodMoves[Math.floor(Math.random() * (goodMoves.length))];
+      this.activateBorder(randomMove.row, randomMove.column, randomMove.border);
+      return;
+    }
+    const randomMove = availableMoves[Math.floor(Math.random() * (availableMoves.length))];
+    this.activateBorder(randomMove.row, randomMove.column, randomMove.border);
+  }
 }
 
 export default Field;
--- src/models/CellModel.js
+++ src/models/CellModel.js
@@ -1,26 +1,29 @@
 class CellModel {
-    constructor(top, right, bottom, left) {
-        this.top = top;
-        this.right = right;
-        this.bottom = bottom;
-        this.left = left;
-        this.player = undefined;
-        this.getOpenBorders = this.getOpenBorders.bind(this);
-    }
+  constructor(top, right, bottom, left) {
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.left = left;
+    this.player = undefined;
+  }
 
-    get isActive() {
-        return this.top === 2 && this.right === 2 && this.bottom === 2 && this.left === 2;
-    }
+  get isActive() {
+    return this.top === 2 && this.right === 2 && this.bottom === 2 && this.left === 2;
+  }
 
-    getOpenBorders() {
-        const openBorders = [];
-        if (this.top === 1) openBorders.push(0);
-        if (this.right === 1) openBorders.push(1);
-        if (this.bottom === 1) openBorders.push(2);
-        if (this.left === 1) openBorders.push(3);
+  get hasMove() {
+    return this.top === 1 || this.right === 1 || this.bottom === 1 || this.left === 1;
+  }
 
-        return openBorders;
-    }
+  getOpenBorders() {
+    const openBorders = [];
+    if (this.top === 1) openBorders.push(0);
+    if (this.right === 1) openBorders.push(1);
+    if (this.bottom === 1) openBorders.push(2);
+    if (this.left === 1) openBorders.push(3);
+
+    return openBorders;
+  }
 }
 
 export default CellModel;
