209f8cbc9b7715a4a1e5462b302eb61431e4a5b8
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2017-12-24 21:35:58 +0100

    Convert Cell to pure component

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,18 +1,18 @@
 import './Cell.css';
 import React from 'react';
 
-const Cell = (props) => {
-  const borderClassName = (border) => {
+class Cell extends React.PureComponent {
+  borderClassName = (border) => {
     let cssClass = border;
-    if (props[border] === 2) {
+    if (this.props[border] === 2) {
       cssClass += ' active';
     }
 
     return cssClass;
   };
 
-  const cellClassName = () => {
-    let { top, right, bottom, left, player } = props;
+  cellClassName = () => {
+    let { top, right, bottom, left, player } = this.props;
     let cssClass = "box";
     if (top === 2 && right === 2 && bottom === 2 && left === 2) {
       cssClass += player === 0 ? ' isFirstPlayer' : ' isSecondPlayer';
@@ -21,27 +21,28 @@ const Cell = (props) => {
     return cssClass;
   }
 
-  const handleClick = function(event, index) {
+  handleClick = (index, event) => {
     event.stopPropagation();
-    props.onBorderClick(index);
+    this.props.onBorderClick(index);
   }
 
-  return (
-    <div className={cellClassName()} onClick={() => props.onClick()}>
-      {props.top > 0 &&
-        <div className={borderClassName('top')} onClick={(e) => handleClick(e, 0)}></div>
-      }
-      {props.right > 0 &&
-        <div className={borderClassName('right')} onClick={(e) => handleClick(e, 1)}></div>
-      }
-      {props.bottom > 0 &&
-        <div className={borderClassName('bottom')} onClick={(e) => handleClick(e, 2)}></div>
-      }
-      {props.left > 0 &&
-        <div className={borderClassName('left')} onClick={(e) => handleClick(e, 3)}></div>
-      }
-    </div>
-  );
+  render() {
+    return (
+      <div className={this.cellClassName()} onClick={() => this.props.onClick()}>
+        {this.props.top > 0 &&
+          <div className={this.borderClassName('top')} onClick={this.handleClick.bind(this, 0)}></div>
+        }
+        {this.props.right > 0 &&
+          <div className={this.borderClassName('right')} onClick={this.handleClick.bind(this, 1)}></div>
+        }
+        {this.props.bottom > 0 &&
+          <div className={this.borderClassName('bottom')} onClick={this.handleClick.bind(this, 2)}></div>
+        }
+        {this.props.left > 0 &&
+          <div className={this.borderClassName('left')} onClick={this.handleClick.bind(this, 3)}></div>
+        }
+      </div>
+    )};
 }
 
 export default Cell;
