5f28fe57a975201a509c1f80295eb65759199944
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2018-01-23 21:33:45 +0100

    Lift state up to Game component

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -2,125 +2,45 @@ import './Field.css';
 import React from 'react';
 import Cell from '../Cell/Cell';
 import { buildRows, getCellNeighbours } from '../../models/FieldModel';
-import computeMove from '../../services/computer';
 
 class Field extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {
-      rows: buildRows(props.size),
-      player: 0,
-      playerOneScore: 0,
-      playerTwoScore: 0,
-      isGameOver: false
-    }
-    this.activateBorder = this.activateBorder.bind(this);
-    this.onCellClick = this.onCellClick.bind(this);
-  }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.renderToggle !== this.props.renderToggle) {
-      this.setState({
-        rows: buildRows(nextProps.size),
-        player: 0,
-        playerOneScore: 0,
-        playerTwoScore: 0,
-        isGameOver: false
-      });
-    }
+    this.handleOnBorderClick = this.handleOnBorderClick.bind(this);
+    this.onCellClick = this.onCellClick.bind(this);
   }
 
   render() {
     return (
       <div className='container'>
-        {this.state.rows.map((cells, rowIndex) =>
+        {this.props.rows.map((cells, rowIndex) =>
           <div className="row" key={rowIndex}>
             {
               cells.map((cell, cellIndex) =>
                 <Cell
                   key={cellIndex}
-                  onBorderClick={(borderIndex) => this.activateBorder(rowIndex, cellIndex, borderIndex)}
+                  onBorderClick={(borderIndex) => this.props.onBorderClick(rowIndex, cellIndex, borderIndex)}
                   onClick={() => this.onCellClick(rowIndex, cellIndex)}
                   {...cell} />)
             }
           </div>)}
-        <p>Current player: {this.state.player}</p>
-        <p>Player one score: {this.state.playerOneScore}</p>
-        <p>Player two score: {this.state.playerTwoScore}</p>
-        {this.state.isGameOver &&
-          <p>Game over! {this.state.playerOneScore > this.state.playerTwoScore ? 'You won :)' : 'Computer won :('}</p>
-        }
       </div>
     );
   }
 
-  activateBorder(row, column, border) {
-    this.setState(prevState => {
-      const rows = prevState.rows.slice().map(r => r.slice());
-      const neighbours = getCellNeighbours(row, column);
-      const neighbour = neighbours[border];
-      const currentCell = rows[row][column];
-      let nextCell;
-      let keepTurn = false;
-      let scoreIncrement = 0;
-
-      if (currentCell[neighbour.borderName] === 2) {
-        return;
-      }
-      nextCell = rows[neighbour.nextRow][neighbour.nextColumn];
-      currentCell[neighbour.borderName] = 2;
-      const nextCellBorderName = neighbours[border > 1 ? border - 2 : border + 2].borderName;
-      nextCell[nextCellBorderName] = 2;
-      if (currentCell.isActive) {
-        currentCell.player = prevState.player;
-        keepTurn = true;
-        scoreIncrement++;
-      }
-      if (nextCell.isActive) {
-        nextCell.player = prevState.player;
-        keepTurn = true;
-        scoreIncrement++;
-      }
-
-      let playerOneScore = prevState.playerOneScore;
-      let playerTwoScore = prevState.playerTwoScore;
-
-      playerOneScore += keepTurn && prevState.player === 0 ? scoreIncrement : 0;
-      playerTwoScore += keepTurn && prevState.player === 1 ? scoreIncrement : 0;
-      
-      let player = keepTurn ? prevState.player : prevState.player === 0 ? 1 : 0;
-
-      let areMovesExist = rows.some(cells => cells.some(cell => cell.hasMove));
-      let isGameOver = prevState.isGameOver;
-      if (areMovesExist) {
-        if (player === 1) {
-          this.computerMove();
-        }
-      } else {
-        isGameOver = true;
-      }
-
-      return { rows, player, playerOneScore, playerTwoScore, isGameOver };
-    });
+  handleOnBorderClick(borderIndex) {
+    //this.props.onBorderClick(rowIndex, cellIndex, borderIndex);
   }
 
   onCellClick(row, column) {
-    const currentCell = this.state.rows[row][column];
+    const currentCell = this.props.rows[row][column];
     const openBorders = currentCell.getOpenBorders();
     if (openBorders.length !== 1) {
       return;
     }
 
-    this.activateBorder(row, column, openBorders[0]);
-  }
-
-  computerMove() {
-    setTimeout(() => {
-      const move = computeMove(this.state.rows);
-      if (move) {
-        this.activateBorder(move.row, move.column, move.border);
-      }
-    }, 100);
+    this.props.onBorderClick(row, column, openBorders[0]);
   }
 }
 
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,41 +1,112 @@
 import React from 'react';
 import Field from '../Field/Field';
+import { buildRows, getCellNeighbours } from '../../models/FieldModel';
+import computeMove from '../../services/computer';
 import './Game.css';
 
 class Game extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {
-      fieldSizeInput: 5,
-      fieldSize: 5,
-      renderToggle: false
-    }
+    this.state = this.constructCleanState(5);
 
     this.reset = this.reset.bind(this);
     this.fieldSizeChange = this.fieldSizeChange.bind(this);
+    this.handleBorderClick = this.handleBorderClick.bind(this);
+  }
+
+  constructCleanState(fieldSize) {
+    return {
+      fieldSize: fieldSize,
+      rows: buildRows(fieldSize),
+      player: 0,
+      playerOneScore: 0,
+      playerTwoScore: 0,
+      isGameOver: false
+    }
   }
 
   fieldSizeChange(event) {
-    this.setState({fieldSizeInput: parseInt(event.target.value, 10)});
+    this.setState(this.constructCleanState(parseInt(event.target.value, 10)));
   }
 
   reset() {
-    this.setState((prevState) => ({
-        fieldSize: prevState.fieldSizeInput,
-        renderToggle: !prevState.renderToggle
-      })
-    );
+    this.setState((prevState) => this.constructCleanState(prevState.fieldSize));
+  }
+
+  handleBorderClick(row, column, border) {
+    this.setState(prevState => {
+      const rows = prevState.rows.slice().map(r => r.slice());
+      const neighbours = getCellNeighbours(row, column);
+      const neighbour = neighbours[border];
+      const currentCell = rows[row][column];
+      let nextCell;
+      let keepTurn = false;
+      let scoreIncrement = 0;
+
+      if (currentCell[neighbour.borderName] === 2) {
+        return;
+      }
+      nextCell = rows[neighbour.nextRow][neighbour.nextColumn];
+      currentCell[neighbour.borderName] = 2;
+      const nextCellBorderName = neighbours[border > 1 ? border - 2 : border + 2].borderName;
+      nextCell[nextCellBorderName] = 2;
+      if (currentCell.isActive) {
+        currentCell.player = prevState.player;
+        keepTurn = true;
+        scoreIncrement++;
+      }
+      if (nextCell.isActive) {
+        nextCell.player = prevState.player;
+        keepTurn = true;
+        scoreIncrement++;
+      }
+
+      let playerOneScore = prevState.playerOneScore;
+      let playerTwoScore = prevState.playerTwoScore;
+
+      playerOneScore += keepTurn && prevState.player === 0 ? scoreIncrement : 0;
+      playerTwoScore += keepTurn && prevState.player === 1 ? scoreIncrement : 0;
+
+      let player = keepTurn ? prevState.player : prevState.player === 0 ? 1 : 0;
+
+      let areMovesExist = rows.some(cells => cells.some(cell => cell.hasMove));
+      let isGameOver = prevState.isGameOver;
+      if (areMovesExist) {
+        if (player === 1) {
+          this.computerMove();
+        }
+      } else {
+        isGameOver = true;
+      }
+
+      return { rows, player, playerOneScore, playerTwoScore, isGameOver };
+    });
+  }
+
+  computerMove() {
+    setTimeout(() => {
+      const move = computeMove(this.state.rows);
+      if (move) {
+        this.handleBorderClick(move.row, move.column, move.border);
+      }
+    }, 100);
   }
 
   render() {
     return (
       <div>
         <div className="FieldSettings">
-          Field size: <input className="FieldSizeInput" type="number" value={this.state.fieldSizeInput} onChange={this.fieldSizeChange} />
+          Field size: <input className="FieldSizeInput" type="number" value={this.state.fieldSize} onChange={this.fieldSizeChange} />
           <button className="ResetButton" onClick={this.reset}>Reset</button>
         </div>
-        <Field size={this.state.fieldSize} renderToggle={this.state.renderToggle} />
+        <Field rows={this.state.rows} onBorderClick={this.handleBorderClick} />
+        <p>Current player: {this.state.player}</p>
+        <p>Player one score: {this.state.playerOneScore}</p>
+        <p>Player two score: {this.state.playerTwoScore}</p>
+        {this.state.isGameOver &&
+          <p>Game over! {this.state.playerOneScore > this.state.playerTwoScore ? 'You won :)' : 'Computer won :('}</p>
+        }
       </div>
     );
   }
