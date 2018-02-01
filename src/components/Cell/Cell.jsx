0449000bf9460fb19a76c80f4c489fd1f9a0c9b5
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2018-02-01 21:15:01 +0100

    Refactor Cell to stateless functional component

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,18 +1,19 @@
 import './Cell.css';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class Cell extends React.PureComponent {
-  borderClassName = (border) => {
+const Cell = (props) => {
+  const borderClassName = (border) => {
     let cssClass = border;
-    if (this.props[border] === 2) {
+    if (props[border] === 2) {
       cssClass += ' active';
     }
 
     return cssClass;
   };
 
-  cellClassName = () => {
-    let { top, right, bottom, left, player } = this.props;
+  const cellClassName = () => {
+    const { top, right, bottom, left, player } = props;
     let cssClass = "box";
     if (top === 2 && right === 2 && bottom === 2 && left === 2) {
       cssClass += player === 0 ? ' isFirstPlayer' : ' isSecondPlayer';
@@ -21,28 +22,38 @@ class Cell extends React.PureComponent {
     return cssClass;
   }
 
-  handleClick = (index, event) => {
+  const handleClick = (index, event) => {
     event.stopPropagation();
-    this.props.onBorderClick(index);
+    props.onBorderClick(index);
+    console.log(index)
   }
 
-  render() {
-    return (
-      <div className={this.cellClassName()} onClick={() => this.props.onClick()}>
-        {this.props.top > 0 &&
-          <div className={this.borderClassName('top')} onClick={this.handleClick.bind(this, 0)}></div>
-        }
-        {this.props.right > 0 &&
-          <div className={this.borderClassName('right')} onClick={this.handleClick.bind(this, 1)}></div>
-        }
-        {this.props.bottom > 0 &&
-          <div className={this.borderClassName('bottom')} onClick={this.handleClick.bind(this, 2)}></div>
-        }
-        {this.props.left > 0 &&
-          <div className={this.borderClassName('left')} onClick={this.handleClick.bind(this, 3)}></div>
-        }
-      </div>
-    )};
+  return (
+    <div className={cellClassName()} onClick={() => props.onClick()}>
+      {props.top > 0 &&
+        <div className={borderClassName('top')} onClick={handleClick.bind(null, 0)}></div>
+      }
+      {props.right > 0 &&
+        <div className={borderClassName('right')} onClick={handleClick.bind(null, 1)}></div>
+      }
+      {props.bottom > 0 &&
+        <div className={borderClassName('bottom')} onClick={handleClick.bind(null, 2)}></div>
+      }
+      {props.left > 0 &&
+        <div className={borderClassName('left')} onClick={handleClick.bind(null, 3)}></div>
+      }
+    </div>
+  );
 }
+
+Cell.propTypes = {
+  top: PropTypes.number,
+  right: PropTypes.number,
+  bottom: PropTypes.number,
+  left: PropTypes.number,
+  player: PropTypes.number,
+  onBorderClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Cell;
