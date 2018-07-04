7200a211ef6f2514aa89e58a409209b8b27979dd
Ilya Luzyanin <ilya.luzyanin@gmail.com>
2018-07-04 22:34:59 +0200

    Refactor Score to a functional component

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './Score.css';
 
-class Score extends React.PureComponent {
-  render() {
-    return (
-      <div className="Score">
-        <span className="Score-firstPlayer">{this.props.playerOneScore}</span>:
-        <span className="Score-secondPlayer">{this.props.playerTwoScore}</span>
-      </div>
-    );
-  }
-}
+const Score = (props) => (
+  <div className="Score">
+    <span className="Score-firstPlayer">{props.playerOneScore}</span>:
+    <span className="Score-secondPlayer">{props.playerTwoScore}</span>
+  </div>
+);
 
 export default Score;
